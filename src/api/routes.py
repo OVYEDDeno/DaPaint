315c64eedf1970452df5c3b5cfd8dca92fc036cb
OVYED Deno <nevad34@gmail.com>
2024-10-19 00:18:19 -0400

    10/19

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, abort
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
+from api import send_email
 from api.models import db, InviteCode, User, DaPaint, UserImg, Notifications, Insight, UserDisqualification, Reports, invitee_association, Feedback, Ticket, Orders
 from flask_cors import CORS
 from datetime import datetime, date, timedelta
